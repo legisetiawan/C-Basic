2184a485f66661c6e53447a200efd06502ebd661
legisetiawan <legi.setiawan@gmail.com>
2024-10-02 12:48:37 +0700

    operator penugasan, = tambah,kurang, kali ,bagi, modulus, dan, atau, tah apoo.!! (^)

--- C# Basic/Program.cs
+++ C# Basic/Program.cs
@@ -32,23 +32,71 @@ namespace C__Basic
             //Console.ReadLine();
 
             //Operator matematika
-            int angkaPertama = 32;
-            int angkaKedua = 23;
-            int hasilTambah = angkaPertama + angkaKedua;
-            int hasilKurang = angkaPertama - angkaKedua;
-            int hasilBagi = angkaPertama / angkaKedua; /* Pastikan type data hasil operator sesuai keinginan karna dari hasil bagi, kali bisa menjadi bil pecahan*/
-            int hasilkali = angkaPertama * angkaKedua; /* Pastikan type data hasil operator sesuai keinginan karna dari hasil bagi, kali bisa menjadi bil pecahan*/
-            angkaPertama++;/*bertambah 1*/
-            angkaKedua--; /*berkurang 1*/
-            Console.WriteLine(hasilTambah);
-            Console.WriteLine(hasilKurang);
-            Console.WriteLine(hasilBagi);
-            Console.WriteLine(hasilkali);
-            Console.WriteLine(angkaPertama % angkaKedua); /* hasil sisa pembagian dua bilangan*/
-            Console.WriteLine(angkaPertama);
-            Console.WriteLine(angkaKedua);
-            Console.ReadLine();
-                   
+            //int angkaPertama = 32;
+            //int angkaKedua = 23;
+            //int hasilTambah = angkaPertama + angkaKedua;
+            //int hasilKurang = angkaPertama - angkaKedua;
+            //int hasilBagi = angkaPertama / angkaKedua; /* Pastikan type data hasil operator sesuai keinginan karna dari hasil bagi, kali bisa menjadi bil pecahan*/
+            //int hasilkali = angkaPertama * angkaKedua; /* Pastikan type data hasil operator sesuai keinginan karna dari hasil bagi, kali bisa menjadi bil pecahan*/
+            //angkaPertama++;/*bertambah 1*/
+            //angkaKedua--; /*berkurang 1*/
+            //Console.WriteLine(hasilTambah);
+            //Console.WriteLine(hasilKurang);
+            //Console.WriteLine(hasilBagi);
+            //Console.WriteLine(hasilkali);
+            //Console.WriteLine(angkaPertama % angkaKedua); /* hasil sisa pembagian dua bilangan*/
+            //Console.WriteLine(angkaPertama);
+            //Console.WriteLine(angkaKedua);
+            //Console.ReadLine();
+
+
+            //Operator Penugasan
+
+            // 1. =
+            int a = 3;
+
+            // 2. +=
+            int b = 3;
+            b += 5;
+
+            // 3. -=
+            int c = 8;
+            c -= 2; 
+
+            // 4. *=
+            int d = 3;
+            d *= 5;
+
+            // 4. /=
+            int e = 8;
+            e /= 2;
+
+            // 4. %=
+            int f = 10;
+            f %= 5;
+
+            // 4. &=
+            int g = 3;
+            g &= 5;
+            
+            // 4. |=
+            int h = 3;
+            h |= 5;
+            
+            // 4. ^=
+            int i = 3;
+            i ^= 5;
+
+            Console.WriteLine("  " + a + " samadengan = ");           
+            Console.WriteLine("  " + b + " tambah");            
+            Console.WriteLine("  " + c + " kurang");            
+            Console.WriteLine("  " + d + " kali");           
+            Console.WriteLine("  " + e + " bagi ");           
+            Console.WriteLine("  " + f + " Mudolus");           
+            Console.WriteLine("  " + g + " dan");           
+            Console.WriteLine("  " + h + " atau");           
+            Console.WriteLine("  " + i + " ^ ntah apo.!!");           
+            Console.ReadLine();           
                 
 
 
